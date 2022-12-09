1b95d5be071e36aa20b4f5ee7c04591d45918073
VURU <sueleyman.vurucu@amova.eu>
2022-12-09 22:53:37 +0100

    inital

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/suikast42/logunifier
+go 1.19
+
+require github.com/trivago/grok v1.0.0
+
+require github.com/trivago/tgo v1.0.7 // indirect
---
+++ patterns/patternfactory.go
@@ -0,0 +1,137 @@
+package patterns
+
+import (
+	"errors"
+	"fmt"
+	"github.com/trivago/grok"
+	additionalPatterns "github.com/trivago/grok/patterns"
+	"strings"
+)
+
+var APPLOGS = map[string]string{
+	"MULTILINE": `((\s)*(.*))*`,
+	"MSG":       `%{MULTILINE:message}`,
+	"NOMAD_LOG": `%{TIMESTAMP_ISO8601:timestamp} \[%{LOGLEVEL:level}\] %{MULTILINE:message}`,
+}
+
+type PatternFactory struct {
+	patterns  map[string]string
+	compilers map[string]*grok.CompiledGrok
+}
+
+func New() (*PatternFactory, error) {
+	// end::tagname[]
+	addPatterns := make(map[string]string)
+	compiledPatterns := make(map[string]*grok.CompiledGrok)
+	{
+		err := add(addPatterns, grok.DefaultPatterns)
+		// better defined in additionalPatterns.Grok
+		for k, _ := range additionalPatterns.Grok {
+			val := grok.DefaultPatterns[k]
+			if len(val) > 0 {
+				delete(addPatterns, k)
+			}
+		}
+		if err != nil {
+			panic(err)
+		}
+	}
+	{
+		err := add(addPatterns, additionalPatterns.Grok)
+		if err != nil {
+			panic(err)
+		}
+	}
+	{
+		err := add(addPatterns, APPLOGS)
+		if err != nil {
+			panic(err)
+		}
+	}
+	//{
+	//	err := add(addPatterns, additionalPatterns.Java)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//{
+	//	err := add(addPatterns, additionalPatterns.LinuxSyslog)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//{
+	//	err := add(addPatterns, additionalPatterns.PostgreSQL)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//{
+	//	err := add(addPatterns, additionalPatterns.Rails)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//{
+	//	err := add(addPatterns, additionalPatterns.Redis)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//{
+	//	err := add(addPatterns, additionalPatterns.Ruby)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//{
+	//	err := add(addPatterns, NGNIX)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	config := grok.Config{
+		Patterns:            addPatterns,
+		SkipDefaultPatterns: true,
+	}
+	g, err := grok.New(config)
+	if err != nil {
+		suberror := errors.New(fmt.Sprintf("Can't create new isnatce\n%s", err.Error()))
+		return nil, suberror
+	}
+	for k, v := range addPatterns {
+		compiled, err := g.Compile(v)
+		if err != nil {
+			suberror := errors.New(fmt.Sprintf("Cannot compile %s with value %s\n%s", k, v, err.Error()))
+			return nil, suberror
+		}
+		compiledPatterns[k] = compiled
+	}
+
+	return &PatternFactory{
+		patterns:  addPatterns,
+		compilers: compiledPatterns,
+	}, nil
+}
+
+func add(source map[string]string, new map[string]string) error {
+
+	for k, v := range new {
+		//key := prefix + "_" + k
+		key := k
+		// IF the key is already present and the default is different
+		if val, ok := source[key]; ok && !strings.EqualFold(val, v) {
+			return errors.New(fmt.Sprintf("%s already exists for value %s and %s should aded", key, v, val))
+		}
+		source[key] = v
+	}
+	return nil
+}
+
+func (factory *PatternFactory) Parse(patternkey string, text string) (map[string]string, error) {
+	compiledGrok := factory.compilers[patternkey]
+	if compiledGrok == nil {
+		return nil, errors.New(fmt.Sprintf("No compiler found for key %s", patternkey))
+	}
+	return compiledGrok.ParseString(text), nil
+}
